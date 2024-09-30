97c0e184da9fc4d4b33782d13511173420fc16b0
Sebastian Poblete <pseba9006@gmail.com>
2024-09-29 22:58:20 -0300

    a

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clientId: '1015042942991-uh57i4cfk6br9ro7s0bmt284s5moi845.apps.googleusercontent.com',
+    clientSecret: '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',
+    redirectUri: 'http://localhost:3000/oauth2/callback',
+  };
+  
